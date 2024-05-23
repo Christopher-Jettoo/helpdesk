727ef42d23dda37eaa8c178cb796914ffb88f3f5
ChrisJettoo <cjettoostudent@aol.com>
2024-05-23 13:43:40 -0400

    update vercel json file

--- server/index.js
+++ server/index.js
@@ -16,30 +16,30 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/tickets', (req, res) => {
-  const ticket = { id: idCounter++, status: 'new', ...req.body };
-  tickets.push(ticket);
-  console.log(`Would normally send email here with body: ${JSON.stringify(ticket)}`);
-  res.status(201).json(ticket);
-});
+// app.post('/tickets', (req, res) => {
+//   const ticket = { id: idCounter++, status: 'new', ...req.body };
+//   tickets.push(ticket);
+//   console.log(`Would normally send email here with body: ${JSON.stringify(ticket)}`);
+//   res.status(201).json(ticket);
+// });
 
-app.get('/tickets', (req, res) => {
-  res.json(tickets);
-});
+// app.get('/tickets', (req, res) => {
+//   res.json(tickets);
+// });
 
-app.get('/tickets/:id', (req, res) => {
-  const ticket = tickets.find(t => t.id === parseInt(req.params.id));
-  if (!ticket) return res.status(404).send('Ticket not found');
-  res.json(ticket);
-});
+// app.get('/tickets/:id', (req, res) => {
+//   const ticket = tickets.find(t => t.id === parseInt(req.params.id));
+//   if (!ticket) return res.status(404).send('Ticket not found');
+//   res.json(ticket);
+// });
 
-app.put('/tickets/:id', (req, res) => {
-  const ticket = tickets.find(t => t.id === parseInt(req.params.id));
-  if (!ticket) return res.status(404).send('Ticket not found');
-  ticket.status = req.body.status || ticket.status;
-  ticket.response = req.body.response || ticket.response;
-  res.json(ticket);
-});
+// app.put('/tickets/:id', (req, res) => {
+//   const ticket = tickets.find(t => t.id === parseInt(req.params.id));
+//   if (!ticket) return res.status(404).send('Ticket not found');
+//   ticket.status = req.body.status || ticket.status;
+//   ticket.response = req.body.response || ticket.response;
+//   res.json(ticket);
+// });
 
 // app.listen(port, () => {
 //   console.log(`Express server listening on port ${port}`);
